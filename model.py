d8abc8d6dc3c7152561728264afe6803f7da6a83
zrui0222 <124349348+zrui0222@users.noreply.github.com>
2024-02-28 16:10:26 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 11:22:32 2024
+
+@author: liuzhiyan
+"""
+
+import random
+
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+start_epoch=1
+
+batch_size = 16
+Nc = 32  # The number of subcarriers
+Nt = 32  # The number of transmit antennas
+N_channel = 2  # Real, Imaginary
+
+# encoded_dim = 32  # dimension of the codeword
+# revise 20230525: erase useless constant variable 'encoded_dim' declared in this file.
+img_total = Nc*Nt*N_channel
+# network params
+residual_num = 2
+encoded_dim = 32  #compress rate=1/4->dim.=512, compress rate=1/16->dim.=128, compress rate=1/32->dim.=64, compress rate=1/64->dim.=32
+
+
+
+
+
+def channel_visualization(image):
+    fig, ax = plt.subplots()
+    plot = ax.imshow(image, cmap=plt.cm.gray, interpolation='nearest', origin='upper')
+    plt.colorbar(plot)
+    ax.spines['left'].set_position(('outward', 10))
+    ax.spines['bottom'].set_position(('outward', 10))
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+    plt.show()
+
+
+def show(image_torch):
+    image = image_torch[4][0].detach().cpu().numpy()
+    channel_visualization(image)
+
+def cosine_decay(init, epoch, total_epoch):
+    epoch = min(epoch, total_epoch)
+    cosine_decay = 0.5 * (1 + math.cos(np.pi * epoch / total_epoch))
+    
+    return init * cosine_decay
+
+class Encoder(nn.Module):
+    # input: (batch_size, Nc, Nt) channel matrix
+    # output: (batch_size, encoded_dim) codeword
+    # CSI_NET
+    def __init__(self, encoded_dim):
+        super().__init__()
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(2, 2, kernel_size=3, stride=1,
+                      padding=1, bias=True),
+            nn.BatchNorm2d(num_features=2),
+            nn.LeakyReLU(negative_slope=0.3),
+        )
+        self.fc = nn.Linear(in_features=2 * Nc * Nt, out_features=encoded_dim)
+        self.leakyrelu = nn.LeakyReLU()
+
+    def forward(self, x, test=False):
+        out = self.conv_block(x)
+        # out.shape = (batch_size, 2, Nc, Nt)
+        # if test: show(out)
+        out = torch.reshape(out, (batch_size, -1))
+        # out.shape = (batch_size, 2*Nc*Nt)
+        out = self.fc(out)
+        # if test: show(torch.reshape(out, (batch_size, 1, 4, encoded_dim//4)))
+        # out.shape = (batch_size, encoded_dim)
+
+        return out
+
+
+class Refinenet(nn.Module):
+    # input: (batch_size, 2, Nc, Nt)
+    # output: (batch_size, 2, Nc, Nt)
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(2, 8, kernel_size=3, stride=1,
+                      padding=1, bias=True),
+            nn.BatchNorm2d(num_features=8),
+            nn.LeakyReLU(negative_slope=0.3),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(8, 16, kernel_size=3, stride=1,
+                      padding=1, bias=True),
+            nn.BatchNorm2d(num_features=16),
+            nn.LeakyReLU(negative_slope=0.3),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(16, 2, kernel_size=3, stride=1,
+                      padding=1, bias=True),
+            nn.BatchNorm2d(num_features=2),
+            nn.LeakyReLU(negative_slope=0.3),
+        )
+
+    def forward(self, x):
+        skip_connection = x
+        out = self.conv1(x)
+        # out.shape = (batch_size, 8, Nc, Nt)
+        out = self.conv2(out)
+        # out.shape = (batch_size, 16, Nc, Nt)
+        out = self.conv3(out)
+        # out.shape = (batch_size, 2, Nc, Nt)
+        out = out + skip_connection
+
+        return out
+
+
+class Decoder(nn.Module):
+    # input: (batch_size, encoded_dim) codeword
+    # output: (batch_size, Nc, Nt) reconstructed channel matrix
+    # CSI_NET
+    def __init__(self, encoded_dim, test=False):
+        super().__init__()
+        self.fc = nn.Linear(in_features=encoded_dim, out_features=2 * Nc * Nt)
+        self.refine1 = Refinenet()
+        self.refine2 = Refinenet()
+        self.test = test
+
+    def forward(self, x, test=False):
+        # x.shape = (batch_size, encoded_dim)
+        out = self.fc(x)
+        # out.shape = (batch_size, 2*Nc*Nt)
+        out = torch.reshape(out, (batch_size, 2, Nc, Nt))
+        # if test: show(out)
+        # out.shape = (batch_size, 2, Nc, Nt)
+        out = self.refine1(out)
+        # if test: show(out)
+        # out.shape = (batch_size, 2, Nc, Nt)
+        out = self.refine2(out)
+        # if test: show(out)
+        # out.shape = (batch_size, 2, Nc, Nt)
+
+        # channel_real = out[:, 0, :, :]
+        # channel_imag = out[:, 1, :, :]
+        # out = channel_real + 1j * channel_imag
+        # out.shape = (batch_size, Nc, Nt)
+        return out
+
+class SeriesAdditionalBlock(nn.Module):
+    # input: (batch_size, 2, Nc, Nt) initial reconstructed CSI
+    # output: (batch_size, 2, Nc, Nt) final reconstructed CSI
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=2 * Nc * Nt, out_features=2 * Nc * Nt),
+            nn.BatchNorm1d(num_features=2 * Nc * Nt),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=2 * Nc * Nt, out_features=2 * Nc * Nt),
+            nn.BatchNorm1d(num_features=2 * Nc * Nt),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=2 * Nc * Nt, out_features=2 * Nc * Nt),
+        )
+
+    def forward(self, x, test=False):
+        x = torch.reshape(x, (batch_size, -1))
+        out = self.fc(x)
+        out = out + x  # skip connection
+        out = torch.reshape(out, (batch_size, 2, Nc, Nt))
+        return out
+   
---
+++ train.py
@@ -0,0 +1,365 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 17:16:53 2024
+
+@author: liuzhiyan
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 16:56:47 2024
+
+@author: liuzhiyan
+"""
+
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from data import load_data
+from model import Encoder, Decoder, SeriesAdditionalBlock
+
+
+import random
+import os
+import time
+import sys
+import argparse
+
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from pathlib import Path
+from os.path import join
+
+
+from tempbalance import Tempbalance
+from sgdsnr import SGDSNR
+from adamp import SGDP, AdamP
+import config as cf
+import torch_optimizer
+from lars_optim import LARS, LAMB
+from utils import train, test, getNetwork, save_args_to_file
+
+from scipy import io as scio
+
+
+
+
+gpu_list = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = gpu_list
+
+
+start_epoch=1
+
+
+Nc = 32  # The number of subcarriers 子载波数量
+Nt = 32  # The number of transmit antennas 发射天线数量
+N_channel = 2  # Real, Imaginary 实部虚部
+
+# encoded_dim = 32  # dimension of the codeword
+# revise 20230525: erase useless constant variable 'encoded_dim' declared in this file.
+img_total = Nc*Nt*N_channel
+# network params
+batch_siza = 128    # 原始为128
+epochs = 1000
+print_freq= 100    # 每100个数据输出一次
+residual_num = 2
+encoded_dim = 32  #compress rate=1/4->dim.=512, compress rate=1/16->dim.=128, compress rate=1/32->dim.=64, compress rate=1/64->dim.=32
+# encoded_dim = 32  # dimension of the codeword
+# revise 20230525: erase useless constant variable 'encoded_dim' declared in this file.
+#NUM_FEEDBACK_BITS = 128   # 反馈数据比特数
+
+
+
+
+
+
+#损失函数
+def NMSE(x, x_hat):
+    x_test_real = torch.reshape(x[:, 0, :, :], (len(x), -1))
+    x_test_imag = torch.reshape(x[:, 1, :, :], (len(x), -1))
+    x_hat_real = torch.reshape(x_hat[:, 0, :, :], (len(x_hat), -1))
+    x_hat_imag = torch.reshape(x_hat[:, 1, :, :], (len(x_hat), -1))
+    power = torch.sum(x_test_real ** 2 + x_test_imag ** 2, axis=1)
+    mse = torch.sum((x_test_real - x_hat_real) ** 2 + (x_test_imag - x_hat_imag) ** 2, axis=1)
+    nmse = torch.mean(mse / power)
+    # print(power)
+    # print(mse)
+    return nmse
+
+#设置初始随机值
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+#通道可视化
+def channel_visualization(image):
+    fig, ax = plt.subplots()
+    plot = ax.imshow(image, cmap=plt.cm.gray, interpolation='nearest', origin='upper')
+    plt.colorbar(plot)
+    ax.spines['left'].set_position(('outward', 10))
+    ax.spines['bottom'].set_position(('outward', 10))
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+    plt.show()
+
+
+#训练模块
+class train(nn.Module):
+    def __init__(self,
+                 envir,
+                 file_path_train,
+                 file_path_val,
+                 file_path_test,
+                 epochs,
+                 encoded_dim,
+                 learning_rate,
+                 lr_decay_freq,
+                 lr_decay,
+                 print_freq,
+                 device,
+                 encoder=None,
+                 decoder=None,
+                 ):
+        super().__init__()
+        self.epochs = epochs
+        self.encoded_dim = encoded_dim
+        self.learning_rate = learning_rate
+        self.lr_decay_freq = lr_decay_freq
+        self.lr_decay = lr_decay
+        self.print_freq = print_freq
+        
+
+        #### 1.load data ####        
+        #数据加载        
+        if envir == 'indoor':
+            self.train_loader = scio.loadmat(file_path_train) ['HT'] # array 'data/DATA_Htrainin.mat'
+            self.val_loader = scio.loadmat(file_path_val)['HT'] #'data/DATA_Hvalin.mat'
+            self.test_loader = scio.loadmat(file_path_test)['HT']  #'data/DATA_Htestin.mat'
+
+        elif envir == 'outdoor':
+            self.train_loader = scio.loadmat(file_path_train) ['HT'] # array 'data/DATA_Htrainout.mat'
+            self.val_loader = scio.loadmat(file_path_val)['HT'] #'data/DATA_Hvalout.mat'
+            self.test_loader = scio.loadmat(file_path_test)['HT']  #'data/DATA_Htestout.mat'
+
+        self.train_loader  = self.train_loader .astype('float32')
+        self.val_loader = self.val_loader.astype('float32')
+        self.test_loader = self.test_loader('float32')
+        self.train_loader  = np.reshape(self.train_loader , (len(self.train_loader ), 2, 32, 32))  
+        self.val_loader  = np.reshape(self.val_loader , (len(self.val_loader ), 2, 32, 32)) 
+        self.test_loader  = np.reshape(self.test_loader , (len(self.test_loader ), 2, 32, 32)) 
+       
+        self.encoder_ue = Encoder(encoded_dim).to(device)
+        self.decoder_bs = Decoder(encoded_dim).to(device)
+        self.additional_block = SeriesAdditionalBlock().to(device)
+        if encoder is not None:
+            self.encoder_ue.load_state_dict(encoder)  #加载与保存权重
+        if decoder is not None:
+            self.decoder_bs.load_state_dict(decoder)
+
+        self.criterion = nn.MSELoss()
+        
+        #优化器选择
+        #ue优化器
+        self.tb_scheduler_ue = Tempbalance(net=self.encoder_ue, 
+                        pl_fitting='median',
+                        xmin_pos=2, 
+                        filter_zeros=False,
+                        remove_first_layer=True,
+                        remove_last_layer=True,
+                        esd_metric_for_tb='alpha',
+                        assign_func='tb_linear_map',
+                        lr_min_ratio=0.5,
+                        lr_max_ratio=1.5,
+                        batchnorm=True,
+                        batchnorm_type='name'
+                        )
+        self.tb_param_group_ue, _ = \
+            self.tb_scheduler_ue.build_optimizer_param_group(untuned_lr=0.01, initialize=True)
+        self.optimizer_ue = optim.SGD(self.tb_param_group_ue,momentum=0.9, weight_decay=1e-4)
+        
+        #bs优化器
+        self.tb_scheduler_bs = Tempbalance(net=self.decoder_bs, 
+                        pl_fitting='median',
+                        xmin_pos=2, 
+                        filter_zeros=False,
+                        remove_first_layer=True,
+                        remove_last_layer=True,
+                        esd_metric_for_tb='alpha',
+                        assign_func='tb_linear_map',
+                        lr_min_ratio=0.5,
+                        lr_max_ratio=1.5,
+                        batchnorm=True,
+                        batchnorm_type='name'
+                        )
+        self.tb_param_group_bs, _ = \
+            self.tb_scheduler_bs.build_optimizer_param_group(untuned_lr=0.01, initialize=True)
+        self.optimizer_bs = optim.SGD(self.tb_param_group_bs,momentum=0.9, weight_decay=1e-4)
+        
+        #ad优化器
+        self.tb_scheduler_ad = Tempbalance(net=self.additional_block, 
+                        pl_fitting='median',
+                        xmin_pos=2, 
+                        filter_zeros=False,
+                        remove_first_layer=True,
+                        remove_last_layer=True,
+                        esd_metric_for_tb='alpha',
+                        assign_func='tb_linear_map',
+                        lr_min_ratio=0.5,
+                        lr_max_ratio=1.5,
+                        batchnorm=True,
+                        batchnorm_type='name'
+                        )
+        self.tb_param_group_ad, _ = \
+            self.tb_scheduler_ad.build_optimizer_param_group(untuned_lr=0.01, initialize=True)
+        self.optimizer_ad = optim.SGD(self.tb_param_group_ad,momentum=0.9, weight_decay=1e-4)
+
+
+        SEED = 42
+        seed_everything(SEED)
+
+    def train_epoch(self):
+
+        self.encoder_ue.train()
+        self.decoder_bs.train()
+
+        #### 2. train_epoch ####
+        for epoch in range(self.epochs):
+
+#             __________                           __________
+#             |         \                         /         |
+#             |          |                       |          |
+# input ----> |encoder_ue| ----> codeword ---->  |decoder_bs| ----> output ----------
+# (2,Nc,Nt)   |          |     (encoded_dim)     |          |      (2,Nc,Nt)        |
+#   |         |_________/                         \_________|                       v
+#   |                                                                              MSE
+#   |                                                                               ^
+#   |_______________________________________________________________________________|
+
+            for i, input in enumerate(self.train_loader):
+                input = input.cuda()
+                codeword = self.encoder_ue(input)
+                output = self.decoder_bs(codeword)
+
+                loss = self.criterion(output, input)
+                loss.backward()
+                
+                self.untuned_lr = cf.cosine_decay(0.01, epoch, self.epochs)
+                self.tb_scheduler_ue.step(self.optimizer_ue, self.untuned_lr)
+                self.tb_scheduler_bs.step(self.optimizer_bs, self.untuned_lr)
+                self.tb_scheduler_ad.step(self.optimizer_ad, self.untuned_lr)
+                
+
+                if i % self.print_freq == 0:
+                    print('Epoch: [{0}][{1}/{2}]\t'
+                          'Loss {loss:.4f}\t'.format(
+                        epoch, i, len(self.train_loader), loss=loss.item()))
+
+            #### 3. validate ####
+        self.encoder_ue.eval()
+        self.decoder_bs.eval()
+
+        total_loss = 0
+        start = time.time()
+        with torch.no_grad():
+            for i, input in enumerate(self.test_loader):
+                input = input.cuda()
+                codeword = self.encoder_ue(input, test=True)
+                output = self.decoder_bs(codeword, test=True)
+                #total_loss += self.criterion(output_additional, input).item()
+                total_loss += NMSE(output, input) 
+
+            end = time.time()
+            t = end - start
+            average_loss = total_loss / len(list(enumerate(self.test_loader)))
+            print('NMSE %.6ftime %.3f' % (average_loss, t))
+
+        channel_visualization(input.detach().cpu().numpy()[12][0])
+        channel_visualization(output.detach().cpu().numpy()[12][0])
+
+        torch.save(self.encoder_ue.state_dict(), './trained_models/encoder_ue_pretrain.pt')
+        torch.save(self.decoder_bs.state_dict(), './trained_models/decoder_bs_pretrain.pt')
+
+        return self.encoder_ue.state_dict(), self.decoder_bs.state_dict()
+
+    def train_online_epoch(self):
+
+        self.encoder_ue.eval()
+        self.decoder_bs.train()
+        self.additional_block.train()
+
+        #### 2. train_epoch ####
+        for epoch in range(self.epochs):
+
+#           __________                       __________                ________                    __________
+#           |         \                     /         |                |      |                    |         \
+#           |          |                   |          |                |addi  |       output_      |          |
+# input --> |encoder_ue| --> codeword -->  |decoder_bs| --> output  -->|tional| --> additional --> |encoder_ue| --> estimated_codeword
+# (2,Nc,Nt) |          |   (encoded_dim)   |          |    (2,Nc,Nt)   |block |       (2,Nc,Nt)    |          |          (encoded_dim)
+#           |_________/         |           \_________|                |______|                    |_________/               |
+#            cannot be          |             can be                    can be                      cannot be                V
+#             trained           |            trained                   trained                       trained                MSE
+#                               |                                                                                            ^
+#                               |____________________________________________________________________________________________|
+
+            for i, input in enumerate(self.train_loader):
+                input = input.cuda()
+                codeword = self.encoder_ue(input)
+                output = self.decoder_bs(codeword)
+                output_additional = self.additional_block(output)
+                estimated_codeword = self.encoder_ue(output_additional)
+
+                loss = self.criterion(estimated_codeword, codeword) + 0.5 * self.criterion(output_additional, output)
+                loss.backward()
+                
+                
+                self.untuned_lr = cf.cosine_decay(0.01, epoch, self.epochs)
+                self.tb_scheduler_bs.step(self.optimizer_bs, self.untuned_lr)
+                self.tb_scheduler_ad.step(self.optimizer_ad, self.untuned_lr)
+
+                if i % self.print_freq == 0:
+                    print('Epoch: [{0}][{1}/{2}]\t'
+                          'Loss {loss:.4f}\t'.format(
+                        epoch, i, len(self.train_loader), loss=loss.item()))
+
+            #### 3. validate ####
+        self.encoder_ue.eval()
+        self.decoder_bs.eval()
+        self.additional_block.eval()
+
+        total_loss = 0
+        start = time.time()
+        with torch.no_grad():
+            for i, input in enumerate(self.test_loader):
+                input = input.cuda()
+                codeword = self.encoder_ue(input, test=True)
+                output = self.decoder_bs(codeword, test=True)
+                output_additional = self.additional_block(output, test=True)
+                #total_loss += self.criterion(output_additional, input).item()
+                total_loss += NMSE(output_additional, input) 
+                
+            end = time.time()
+            t = end - start
+            average_loss = total_loss / len(list(enumerate(self.test_loader)))
+            print('NMSE %.6ftime %.3f' % (average_loss, t))
+
+        channel_visualization(input.detach().cpu().numpy()[12][0])
+        channel_visualization(output_additional.detach().cpu().numpy()[12][0])
+
+        torch.save(self.encoder_ue.state_dict(), './trained_models/encoder_ue_pretrain.pt')
+        torch.save(self.decoder_bs.state_dict(), './trained_models/decoder_bs_pretrain.pt')
+
+        return self.encoder_ue.state_dict(), self.decoder_bs.state_dict()
